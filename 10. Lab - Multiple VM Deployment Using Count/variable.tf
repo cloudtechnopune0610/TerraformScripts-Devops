6d0abcc57524d29a46a9cff7d2ff61efca5e79c9
cloudtechnopune0610 <103561134+cloudtechnopune0610@users.noreply.github.com>
2022-10-19 23:54:38 +0530

    Update variable.tf

--- 10. Lab - Multiple VM Deployment Using Count/variable.tf
+++ 10. Lab - Multiple VM Deployment Using Count/variable.tf
@@ -1,89 +1,27 @@
-variable "resource_group_name" {
-    type = string
-       }
-
-variable "resource_group_location" {
-    type = string
-    
-}
-variable "virtual_network_name" {
-    type = string
-}
-
-variable "address_space" {
-    #type = list
-    
-}
-
-variable "address_prefixes" {
-    #type = list
-    
-}
-
- variable "subscription_id" {
-    type = string
-    
-}
-
-variable "client_id" {
-    type = string
-    
-}
-
-variable "client_secret" {
-    type = string
-    
-}
-
-variable "tenant_id" {
-    type = string
-    
+variable "prefix" {
+  type = string
 }
 
 
-
-variable "subnet_name" {
-    #type = list
-    
+variable "location" {
+  type = string
 }
 
-variable "public_ip" {
-    #type = list
-    
+variable "addressspace1" {
+  type = list(any) 
 }
 
-variable "network_interface" {
-    #type = list
+variable "subnetname" {
+  type = string
+}
+variable "address1" {
+  type = list(any)
 }
 
-variable "config_name" {
-    #type = list
+variable "vmtype" {
+  type = string
 }
 
-variable "network_security_group" {
-    #type = list
+variable "machinecount" {
+  type = number 
 }
-
-variable "avset_name" {
-    #type = list
-}
-
-variable "vm_name" {
-    #type = string
-       }
-       
-variable "osdisk_name" {
-    #type = string
-       }
-
-variable "managed_disk" {
-    #type = string
-       }
-
- variable "computer_name" {
-    #type = string
-       }
-
- variable "resource_prefix" {
-    #type = string
-       }      
